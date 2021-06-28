ad71eda261ac2de63f1b72887fcc483460e20002
Tyler Lentini <tylernlentini@gmail.com>
2021-06-28 18:35:49 -0400

    worked through module

---
+++ Python_practice/Python_practice.py
@@ -0,0 +1,12 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == 'Denver' :
+    print(counties[1])
+
+
+if "El Paso" in counties:
+    print("El Paso is in the list of counties")
+else: 
+    print("El Paso is not in the the list of counties ")
+
+for county in counties:
+    print(counties)
---
+++ Python_practice/py_practice_dictionaries.py
@@ -0,0 +1,9 @@
+voting_data = [ {"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 
+463353}, {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        message = (
+            f'{county_dict} has {value:,} votes'
+
+        )
